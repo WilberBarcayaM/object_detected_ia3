3fd2bd1cc58cb6b95c166438c5df1b696e1ae2bf
Wilber Barcaya Muruchi <barcayamuruchiwilber@gmail.com>
2026-05-30 17:11:57 -0400

    update app

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,26 @@
-import 'dart:async';
-import 'dart:typed_data';
+//import 'dart:io';
+//import 'dart:typed_data';
+import 'dart:ui';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+//import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'dart:async';
 import 'package:flutter_vision/flutter_vision.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+//import 'package:image_picker/image_picker.dart';
 
 late List<CameraDescription> cameras;
 
-enum Options { none, frame }
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Solicitar permiso de Bluetooth en tiempo de ejecución
-  await _requestBluetoothPermission();
-
-  // Inicializar las cámaras disponibles
-  cameras = await availableCameras();
-
   runApp(
     const MaterialApp(
       home: MyApp(),
     ),
   );
-}
-
-Future<void> _requestBluetoothPermission() async {
-  PermissionStatus status = await Permission.bluetooth.request();
-  if (status != PermissionStatus.granted) {
-    // Manejar caso donde el usuario niega los permisos
-    print('Bluetooth permission denied');
-    // Puedes mostrar un diálogo o mensaje informativo al usuario
-  } else {
-    print('Bluetooth permission granted');
-  }
 }
 
 class MyApp extends StatefulWidget {
@@ -48,54 +32,18 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late FlutterVision vision;
-  Options option = Options.none;
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  BluetoothDevice? connectedDevice;
-  BluetoothCharacteristic? characteristic;
-  String distance = "No data";
 
   @override
   void initState() {
     super.initState();
     vision = FlutterVision();
-    startScan();
-  }
-
-  void startScan() {
-    flutterBlue.startScan(timeout: Duration(seconds: 4));
-
-    flutterBlue.scanResults.listen((results) {
-      for (ScanResult r in results) {
-        print("Found device: ${r.device.name}");
-        if (r.device.name == "HC05-wilber") {
-          flutterBlue.stopScan();
-          connectToDevice(r.device);
-          break;
-        }
-      }
+    // Iniciar la detección de objetos al iniciar la aplicación
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => YoloVideo(vision: vision)),
+      );
     });
-  }
-
-  Future<void> connectToDevice(BluetoothDevice device) async {
-    await device.connect();
-    List<BluetoothService> services = await device.discoverServices();
-    for (BluetoothService service in services) {
-      for (BluetoothCharacteristic c in service.characteristics) {
-        if (c.properties.notify) {
-          c.setNotifyValue(true);
-          c.value.listen((value) {
-            setState(() {
-              distance = String.fromCharCodes(value);
-            });
-          });
-          setState(() {
-            connectedDevice = device;
-            characteristic = c;
-          });
-          break;
-        }
-      }
-    }
   }
 
   @override
@@ -106,52 +54,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: task(option),
-      floatingActionButton: SpeedDial(
-        icon: Icons.menu,
-        activeIcon: Icons.close,
-        backgroundColor: Colors.black12,
-        foregroundColor: Colors.white,
-        activeBackgroundColor: Colors.deepPurpleAccent,
-        activeForegroundColor: Colors.white,
-        visible: true,
-        closeManually: false,
-        curve: Curves.bounceIn,
-        overlayColor: Colors.black,
-        overlayOpacity: 0.5,
-        buttonSize: const Size(56.0, 56.0),
-        children: [
-          SpeedDialChild(
-            child: const Icon(Icons.video_call),
-            backgroundColor: Colors.red,
-            foregroundColor: Colors.white,
-            label: 'Detectar Objeto',
-            labelStyle: const TextStyle(fontSize: 18.0),
-            onTap: () {
-              setState(() {
-                option = Options.frame;
-              });
-            },
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: Center(child: Text("Initializing...")),
     );
-  }
-
-  Widget task(Options option) {
-    if (option == Options.frame) {
-      return YoloVideo(vision: vision, distance: distance);
-    }
-    return const Center(child: Text("Choose Task"));
   }
 }
 
 class YoloVideo extends StatefulWidget {
   final FlutterVision vision;
-  final String distance;
-  const YoloVideo({Key? key, required this.vision, required this.distance})
-      : super(key: key);
+  const YoloVideo({Key? key, required this.vision}) : super(key: key);
 
   @override
   State<YoloVideo> createState() => _YoloVideoState();
@@ -163,28 +74,112 @@ class _YoloVideoState extends State<YoloVideo> {
   CameraImage? cameraImage;
   bool isLoaded = false;
   bool isDetecting = false;
+  String detectedObject = "";
+
+  // Variables for Bluetooth connection and sensor data
+  final _bluetooth = FlutterBluetoothSerial.instance;
+  bool _bluetoothState = false;
+  bool _isConnecting = false;
+  BluetoothConnection? _connection;
+  List<BluetoothDevice> _devices = [];
+  BluetoothDevice? _deviceConnected;
+  String ultrasonicValue = '';
+  FlutterTts flutterTts = FlutterTts();
+  Timer? _timer;
+  bool _isSpeakingEnabled = true;
 
   @override
   void initState() {
     super.initState();
     init();
+    _requestPermission();
+    _initBluetooth();
+    _initTts();
+    _startSpeakingTimer();
   }
 
   init() async {
+    cameras = await availableCameras();
     controller = CameraController(cameras[0], ResolutionPreset.low);
-    await controller.initialize();
-    await loadYoloModel();
-    setState(() {
-      isLoaded = true;
-      isDetecting = false;
-      yoloResults = [];
+    controller.initialize().then((value) {
+      loadYoloModel().then((value) {
+        setState(() {
+          isLoaded = true;
+          isDetecting = false;
+          yoloResults = [];
+        });
+        startDetection();
+      });
+    });
+  }
+
+  void _requestPermission() async {
+    await Permission.location.request();
+    await Permission.bluetooth.request();
+    await Permission.bluetoothScan.request();
+    await Permission.bluetoothConnect.request();
+  }
+
+  void _initBluetooth() {
+    _bluetooth.state.then((state) {
+      setState(() => _bluetoothState = state.isEnabled);
+    });
+
+    _bluetooth.onStateChanged().listen((state) {
+      setState(() => _bluetoothState = state.isEnabled);
+    });
+  }
+
+  void _initTts() async {
+    await flutterTts.setLanguage("es-ES");
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setVolume(1.0);
+    await flutterTts.setPitch(1.0);
+  }
+
+  void _startSpeakingTimer() {
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      _speakUltrasonicValue();
+    });
+  }
+
+  Future<void> _speakUltrasonicValue() async {
+    if (ultrasonicValue.isNotEmpty && _isSpeakingEnabled) {
+      await flutterTts.speak(" $ultrasonicValue centímetros");
+    }
+  }
+
+  void _getDevices() async {
+    var res = await _bluetooth.getBondedDevices();
+    setState(() => _devices = res);
+  }
+
+  void _receiveData() {
+    String buffer = '';
+    _connection?.input?.listen((event) {
+      try {
+        String data = String.fromCharCodes(event);
+        buffer += data;
+        if (buffer.contains('\n')) {
+          String value = buffer.substring(0, buffer.indexOf('\n')).trim();
+          setState(() {
+            ultrasonicValue = value;
+          });
+          buffer = '';
+        }
+      } catch (e) {
+        print('Error handling received data: $e');
+      }
     });
   }
 
   @override
   void dispose() async {
-    super.dispose();
+    _timer?.cancel();
+    flutterTts.stop();
+    _connection?.dispose();
     controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -197,61 +192,34 @@ class _YoloVideoState extends State<YoloVideo> {
         ),
       );
     }
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: CameraPreview(controller),
-        ),
-        ...displayBoxesAroundRecognizedObjects(size),
-        Positioned(
-          bottom: 75,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            height: 80,
-            width: 80,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                  width: 5, color: Colors.white, style: BorderStyle.solid),
-            ),
-            child: isDetecting
-                ? IconButton(
-                    onPressed: () async {
-                      stopDetection();
-                    },
-                    icon: const Icon(
-                      Icons.stop,
-                      color: Colors.red,
-                    ),
-                    iconSize: 50,
-                  )
-                : IconButton(
-                    onPressed: () async {
-                      await startDetection();
-                    },
-                    icon: const Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
-                    ),
-                    iconSize: 50,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Sensor Ultrasónico'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                AspectRatio(
+                  aspectRatio: controller.value.aspectRatio,
+                  child: CameraPreview(
+                    controller,
                   ),
-          ),
-        ),
-        Positioned(
-          top: 50,
-          left: 20,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            color: Colors.black54,
-            child: Text(
-              'Distance: ${widget.distance}',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                ...displayBoxesAroundRecognizedObjects(size),
+              ],
             ),
           ),
-        ),
-      ],
+          _controlBT(),
+          _infoDevice(),
+          Expanded(child: _listDevices()),
+          _ultrasonicDisplay(),
+          _toggleSpeakingButton(),
+        ],
+      ),
     );
   }
 
@@ -262,6 +230,9 @@ class _YoloVideoState extends State<YoloVideo> {
         modelVersion: "yolov8",
         numThreads: 6,
         useGpu: true);
+    setState(() {
+      isLoaded = true;
+    });
   }
 
   Future<void> yoloOnFrame(CameraImage cameraImage) async {
@@ -275,6 +246,8 @@ class _YoloVideoState extends State<YoloVideo> {
     if (result.isNotEmpty) {
       setState(() {
         yoloResults = result;
+        detectedObject = result[0]['tag'];
+        print(detectedObject);
       });
     } else {
       setState(() {
@@ -335,5 +308,99 @@ class _YoloVideoState extends State<YoloVideo> {
         ),
       );
     }).toList();
+  }
+
+  Widget _controlBT() {
+    return SwitchListTile(
+      value: _bluetoothState,
+      onChanged: (bool value) async {
+        if (value) {
+          await _bluetooth.requestEnable();
+        } else {
+          await _bluetooth.requestDisable();
+        }
+      },
+      tileColor: Colors.black26,
+      title:
+          Text(_bluetoothState ? "Bluetooth encendido" : "Bluetooth apagado"),
+    );
+  }
+
+  Widget _infoDevice() {
+    return ListTile(
+      tileColor: Colors.black12,
+      title: Text("Conectado a: ${_deviceConnected?.name ?? "ninguno"}"),
+      trailing: _connection?.isConnected ?? false
+          ? TextButton(
+              onPressed: () async {
+                await _connection?.finish();
+                setState(() => _deviceConnected = null);
+              },
+              child: const Text("Desconectar"),
+            )
+          : TextButton(
+              onPressed: _getDevices,
+              child: const Text("Ver dispositivos"),
+            ),
+    );
+  }
+
+  Widget _listDevices() {
+    return _isConnecting
+        ? const Center(child: CircularProgressIndicator())
+        : SingleChildScrollView(
+            child: Container(
+              color: Colors.grey.shade100,
+              child: Column(
+                children: _devices
+                    .map((device) => ListTile(
+                          title: Text(device.name ?? device.address),
+                          trailing: TextButton(
+                            child: const Text('Conectar'),
+                            onPressed: () async {
+                              setState(() => _isConnecting = true);
+                              _connection = await BluetoothConnection.toAddress(
+                                  device.address);
+                              _deviceConnected = device;
+                              _devices = [];
+                              _isConnecting = false;
+                              _receiveData();
+                              setState(() {});
+                            },
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+          );
+  }
+
+  Widget _ultrasonicDisplay() {
+    return ListTile(
+      title: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Text(
+          "Valor Ultrasónico: $ultrasonicValue cm",
+          style: const TextStyle(fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+
+  Widget _toggleSpeakingButton() {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _isSpeakingEnabled = !_isSpeakingEnabled;
+          if (_isSpeakingEnabled) {
+            _startSpeakingTimer();
+          } else {
+            _timer?.cancel();
+            flutterTts.stop();
+          }
+        });
+      },
+      child: Text(_isSpeakingEnabled ? "Desactivar voz" : "Activar voz"),
+    );
   }
 }
